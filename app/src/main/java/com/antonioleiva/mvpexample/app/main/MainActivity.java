f6213ad5d70bf2e128aeb8947da60ba39df62661
TonyZhang <zhangdshr@gmail.com>
2016-03-21 16:20:00 +0800

    first
    first test

--- app/src/main/java/com/antonioleiva/mvpexample/app/main/MainActivity.java
+++ app/src/main/java/com/antonioleiva/mvpexample/app/main/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends Activity implements MainView, AdapterView.OnIt
         listView.setOnItemClickListener(this);
         progressBar = (ProgressBar) findViewById(R.id.progress);
         presenter = new MainPresenterImpl(this);
-
     }
 
     @Override protected void onResume() {
